4a720e02d5b0b324c62170c53f89081b9cfb46b1
MYC-A <149180977+MYC-A@users.noreply.github.com>
2026-05-23 13:18:29 +0000

    Убрал не дефолтные настройки

--- backend_project/app/core/config.py
+++ backend_project/app/core/config.py
@@ -4,28 +4,24 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=(".env", "app/.env"),
+        env_file=".env",
         env_file_encoding="utf-8",
         extra="ignore",
     )
 
-    DATABASE_URL: str = "postgresql+asyncpg://postgres:postgres@localhost:5433/runner_db"
-    DB_USER: str = "postgres"
-    DB_PASSWORD: str = "postgres"
-    DB_HOST: str = "localhost"
-    DB_PORT: str = "5433"
+    DATABASE_URL: str
 
-    SECRET_KEY: str = "change-me-local-secret"
+    SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
-    MINIO_ENDPOINT: str = "localhost:9000"
-    MINIO_PUBLIC_URL: str = "http://localhost:9000"
-    MINIO_ACCESS_KEY: str = "minioadmin"
-    MINIO_SECRET_KEY: str = "minioadmin"
-    MINIO_BUCKET_NAME: str = "photos"
-    MINIO_POSTS_BUCKET_NAME: str = "posts"
-    OPEN_ROUTE_API_KEY: str = ""
+    MINIO_ENDPOINT: str
+    MINIO_PUBLIC_URL: str
+    MINIO_ACCESS_KEY: str
+    MINIO_SECRET_KEY: str
+    MINIO_BUCKET_NAME: str
+    MINIO_POSTS_BUCKET_NAME: str
+    OPEN_ROUTE_API_KEY: str
 
 
 
